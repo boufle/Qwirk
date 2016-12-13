c76fc62f160fe31e15c60b35635119e97c21fed6
Boufle <169154@supinfo.com>
2016-12-13 10:25:06 +0100

    test

--- src/com/supinfo/qwirk/Gui/GuiWindowsFeed/GuiWindowsFeed.java
+++ src/com/supinfo/qwirk/Gui/GuiWindowsFeed/GuiWindowsFeed.java
@@ -1,5 +1,7 @@
 package com.supinfo.qwirk.Gui.GuiWindowsFeed;
 
+import com.supinfo.qwirk.Gui.utils.GuiButton;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,19 +10,38 @@ import java.awt.*;
  */
 public class GuiWindowsFeed extends JPanel {
 
+    JPanel bot = new JPanel();
+    protected JPanel body = new JPanel();
+    protected JPanel pagination = new JPanel();
+    protected GuiButton leftarrow = new GuiButton();
+    protected GuiButton rightarrow = new GuiButton();
+
     public GuiWindowsFeed(){
 
-        prepareFeed(10);
+        setLayout(new BorderLayout());
+        prepareFeed(body);
+        add(body, BorderLayout.CENTER);
+        bot.setPreferredSize(new Dimension(0,69));
+        bot.setBackground(new Color(38,51,67));
+        bot.setLayout(new BorderLayout());
+        pagination.setLayout(new BorderLayout());
+        pagination.setPreferredSize(new Dimension(150,40));
+        pagination.setBackground(new Color(38,51,67));
+        //pagination.add(leftarrow, BorderLayout.LINE_START);
+      //  pagination.add(rightarrow, BorderLayout.LINE_END);
+        bot.add(pagination, BorderLayout.LINE_END);
+        add(bot, BorderLayout.PAGE_END);
+
 
     }
 
-    public void prepareFeed(int numberOfFeed){
+    public void prepareFeed(JPanel body){
 
-        setLayout(new GridLayout(0,3,5,5));
-        for (int i = 0; i < numberOfFeed; i++){
+        body.setLayout(new GridLayout(0,3,5,5));
+        for (int i = 0; i < 12; i++){
 
-            GuiFeed guiFeed = new GuiFeed();
-            add(guiFeed);
+            GuiFeed guiFeed = new GuiFeed("https://image.jimcdn.com/app/cms/image/transf/dimension=360x10000:format=jpg/path/sb9ccc28c2a9d590e/image/ib4429f54ba79584a/version/1318533548/image.jpg", "test");
+            body.add(guiFeed);
         }
     }
 }
